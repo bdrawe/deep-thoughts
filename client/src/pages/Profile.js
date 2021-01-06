2e9d5d5582199ad83fa6d1c2114c11478085c016
B. Drawe <brycerdrawe.b@gmail.com>
2021-01-05 19:49:16 -0700

    thought forms and add friend functionality

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
-
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
-
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { ADD_FRIEND } from '../utils/mutations';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import ThoughtForm from '../components/ThoughtForm';
+
 import Auth from '../utils/auth';
 
 const Profile = props => {
   const { username: userParam } = useParams();
-
+  const [addFriend] = useMutation(ADD_FRIEND)
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   });
@@ -36,6 +37,15 @@ const Profile = props => {
       </h4>
     );
   }
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <div>
@@ -43,11 +53,17 @@ const Profile = props => {
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
+        {userParam && (
+        <button className="btn ml-auto" onClick={handleClick}>
+          Add Friend
+        </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
           <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
+          
         </div>
 
         <div className="col-12 col-lg-3 mb-3">
@@ -58,6 +74,7 @@ const Profile = props => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
